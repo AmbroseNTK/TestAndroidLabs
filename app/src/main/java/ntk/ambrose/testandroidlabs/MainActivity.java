9da59f92ba321e06d79f81555bbf3014c2de166c
Ambrose Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2018-03-19 10:08:26 +0700

    Design UI

--- app/src/main/java/ntk/ambrose/testandroidlabs/MainActivity.java
+++ app/src/main/java/ntk/ambrose/testandroidlabs/MainActivity.java
@@ -1,13 +1,44 @@
 package ntk.ambrose.testandroidlabs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
 
+    Animation animRotate;
+    ImageView imagePokeball;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        animRotate= AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate_anim);
+        animRotate.setDuration(3000);
+        animRotate.setRepeatMode(Animation.RESTART);
+        animRotate.setRepeatCount(Animation.INFINITE);
+
+        animRotate.setAnimationListener(this);
+
+        imagePokeball = findViewById(R.id.imgPokeball);
+        imagePokeball.setAnimation(animRotate);
+
+        //animRotate.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
